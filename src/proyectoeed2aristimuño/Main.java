29ff2fea3cc13bf4a5e991be6d7cdcebbaf1243e
Santiago Aristimuño <sa.aristimuno@correo.unimet.edu.ve>
2023-06-22 20:26:25 -0400

    minor

---
+++ src/proyectoeed2aristimuño/Main.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package proyectoeed2aristimuño;
+import Estructures.HashTable;
+import Windows.MainUI;
+/**
+ *
+ * @author Santiago Aristimuño
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //MainUI x = new MainUI();
+        //x.setVisible(true);
+        HashTable ht = new HashTable();
+        ht.put("A", "1");
+        ht.put("B", "2");
+        ht.put("C", "3");
+        ht.put("D", "4");
+        System.out.println(ht.toString());
+        System.out.println(ht.get("A"));
+    }
+    
+}
