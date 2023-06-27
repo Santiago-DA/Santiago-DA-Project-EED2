374ddc2e2eb496d8d11a5e414fe8cf977350a730
Santiago Aristimuño <sa.aristimuno@correo.unimet.edu.ve>
2023-06-27 15:14:56 -0400

    data in UI

--- src/proyectoeed2aristimuño/Main.java
+++ src/proyectoeed2aristimuño/Main.java
@@ -25,26 +25,35 @@ public class Main {
         Loader loader = new Loader();
         Solution solution = new Solution();
         //loads
-        
         Room[] rooms = (Room[]) loader.loadRooms();
         Reservation[] reservs = (Reservation[]) loader.loadReservations();
         HistorialEntry[] historials = (HistorialEntry[]) loader.loadHistorial();
         Guest[] guests = (Guest[]) loader.loadCurrentGuests();
+        
+        
         //update rooms and BST
         solution.updateRooms(rooms, historials, guests);
         Node root = solution.roomToBST(rooms, 0, rooms.length-1);
         BinarySearchTree<Room> BSTree = new BinarySearchTree();
         BSTree.setRoot(root);
+        
+        
         //Hash table
         HashTable<String, Guest> hashTable = new HashTable<String, Guest>();
         for (Guest guest:guests){
             hashTable.put(guest.getFullName(), guest);
         }
-        System.out.println(reservs[0]);
+        
+        //reservations
+        solution.sortReservations(reservs);
+        
+        
+        
+        
         //next
        
         
-        MainUI x = new MainUI(hashTable);
+        MainUI x = new MainUI(hashTable, reservs,BSTree);
         x.setVisible(true);
     }
     
